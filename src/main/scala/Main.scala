8cf036268b6c5d1e621bb4c4f9755da6c51f23c5
Ahmad Siavashi <siavashi@aut.ac.ir>
2022-03-11 06:14:44 +0330

    Add project files

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,46 @@
+// Use H2Profile to connect to an H2 database
+import slick.jdbc.H2Profile.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+// Definition of the Person table (FI, the alternative is to use the code generator)
+class People(tag: Tag)
+    extends Table[(Int, String, String, Int)](tag, "PEOPLE") {
+  def id = column[Int]("ID", O.PrimaryKey) // This is the primary key column
+  def firstName = column[String]("FIRST_NAME")
+  def lastName = column[String]("LAST_NAME")
+  def age = column[Int]("AGE")
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = (id, firstName, lastName, age)
+}
+
+object Main extends App {
+  val people = TableQuery[People]
+  val db = Database.forConfig("h2mem1")
+  try {
+    val setup = DBIO.seq(
+      // Create the tables
+      (people.schema).create,
+      // Insert some persons
+      people ++= Seq(
+        (0, "Ali", "Rashidi", 25),
+        (1, "Reza", "Javadi", 35),
+        (2, "Narges", "Jamshidi", 27)
+      )
+    )
+    Await.result(db.run(setup), Duration.Inf)
+
+    // Read all people
+    Await.result(
+      db.run(people.result)
+        .map(_.foreach { case (id, firstName, lastName, age) =>
+          println(s"$firstName $lastName is $age years old.")
+        }),
+      Duration.Inf
+    )
+
+  } catch {
+    case e: Throwable => print(e.printStackTrace())
+  } finally db.close
+}
